3ed82a83853f5374b121d3fd6d9f688c8c2ecf2a
jsEklund <eklund84@gmail.com>
2017-03-31 11:08:32 +0200

    Rebuild js-cart with prototypes

--- app/scripts/cart.js
+++ app/scripts/cart.js
@@ -1,72 +1,67 @@
 var Cart = function() {
+    this.cartItems = [];
+};
 
-    var _isIdInArray = function(arr, id) {
-        for (var i = 0; i < arr.length; i++) {
-            if (arr[i].id === id) {
-                return true;
-            }
+Cart.prototype._getPositionInArray = function(arr, id) {
+    var pos;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].id === id) {
+            return pos = i;
         }
-        return false;
     }
+    return pos = -1;
+};
 
-    var _getPositionInArray = function(arr, id) {
-        var pos;
-        for (var i = 0; i < arr.length; i++) {
-            if (arr[i].id === id) {
-                return pos = i;
-            }
+Cart.prototype._isIdInArray = function(arr, id) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].id === id) {
+            return true;
         }
-        return pos = -1;
     }
+    return false;
+};
 
-    return {
+Cart.prototype.addProductCount = function(id, count) {
+    pos = this._getPositionInArray(this.cartItems, id);
+    if (pos !== -1) {
+        this.cartItems[pos].amount += count;
+    }
+}
 
-        cartItems: cartItems = [],
+Cart.prototype.addProduct = function(id, name, amount) {
 
-        addProduct: function(id, name, amount) {
+    var item = {
+        id: id,
+        name: name,
+        amount: amount
+    };
 
-            var item = {
-                id: id,
-                name: name,
-                amount: amount
-            };
+    var pos = this._getPositionInArray(this.cartItems, id);
 
-            var pos = _getPositionInArray(cartItems, id);
-            
-            if (pos !== -1) {
-                cartItems[pos].amount + amount;
-            } else {
-                cartItems.push(item);
-            }
+    if (pos !== -1) {
+        this.cartItems[pos].amount + amount;
+    } else {
+        this.cartItems.push(item);
+    }
+}
 
-        },
+Cart.prototype.removeProductCount = function(id, count) {
+    pos = this._getPositionInArray(this.cartItems, id);
+    if (pos !== -1) {
+        if (this.cartItems[pos].amount > count) {
+            this.cartItems[pos].amount -= count;
+        }
 
-        addProductCount: function(id, count) {
-            pos = _getPositionInArray(cartItems, id);
-            if (pos !== -1) {
-                cartItems[pos].amount += count;
-            }
-        },
+        else if (cartItems[pos].amount >= count) {
+            this.cartItems.splice(pos, 1);
+        }
+    }
+}
 
-        removeProductCount: function(id, count) {
-            pos = _getPositionInArray(cartItems, id);
-            if (pos !== -1) {
-                if (cartItems[pos].amount > count) {
-                    cartItems[pos].amount -= count;
-                }
-
-                else if (cartItems[pos].amount >= count) {
-                    cartItems.splice(pos, 1);
-                }
-            }
-        },
-
-        deleteProduct: function(id) {
-            for (var i = 0; i < cartItems.length; i++) {
-                if (cartItems[i].id === id) {
-                    cartItems.splice(i, 1);
-                }
-            }
+Cart.prototype.deleteProduct = function(id) {
+    for (var i = 0; i < this.cartItems.length; i++) {
+        if (this.cartItems[i].id === id) {
+            this.cartItems.splice(i, 1);
         }
     }
 }
--- app/scripts/test.js
+++ app/scripts/test.js
@@ -1,4 +1,3 @@
-
 var shoppingCart = new Cart();
 
 shoppingCart.addProduct(1, "white T-shirt", 1);
@@ -7,7 +6,7 @@ shoppingCart.addProduct(2, "red T-shirt", 10);
 shoppingCart.addProductCount(2, 10);
 shoppingCart.addProductCount(1, 10);
 
-// shoppingCart.removeProductCount(2, 5);
+shoppingCart.removeProductCount(2, 5);
 // shoppingCart.removeProductCount(1, 6);
 // shoppingCart.removeProductCount(1, 5);
 
